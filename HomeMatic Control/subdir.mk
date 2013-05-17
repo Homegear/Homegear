37abcc12981a2d7321156cf41e4113d284fd2e2b
Sathya <sathya@laufers.net>
2013-05-17 07:25:56 -0700

    Changed ../ to ./

--- HomeMatic Control/subdir.mk
+++ HomeMatic Control/subdir.mk
@@ -4,22 +4,22 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../BidCoSMessage.cpp \
-../BidCoSMessages.cpp \
-../BidCoSPacket.cpp \
-../BidCoSQueue.cpp \
-../BidCoSQueueEntry.cpp \
-../Cul.cpp \
-../Database.cpp \
-../Exception.cpp \
-../HM-CC-TC.cpp \
-../HM-CC-VD.cpp \
-../HM-RC-Sec3-B.cpp \
-../HM-SD.cpp \
-../HomeMaticDevice.cpp \
-../Log.cpp \
-../Peer.cpp \
-../main.cpp 
+./BidCoSMessage.cpp \
+./BidCoSMessages.cpp \
+./BidCoSPacket.cpp \
+./BidCoSQueue.cpp \
+./BidCoSQueueEntry.cpp \
+./Cul.cpp \
+./Database.cpp \
+./Exception.cpp \
+./HM-CC-TC.cpp \
+./HM-CC-VD.cpp \
+./HM-RC-Sec3-B.cpp \
+./HM-SD.cpp \
+./HomeMaticDevice.cpp \
+./Log.cpp \
+./Peer.cpp \
+./main.cpp 
 
 OBJS += \
 ./BidCoSMessage.o \
@@ -59,7 +59,7 @@ CPP_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cpp
+%.o: ./%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
 	g++ -DDEBUG_LEVEL=5 -O3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
