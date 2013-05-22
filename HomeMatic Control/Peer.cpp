d3b45ee45da0dedb2d40c8110c44ed3ff2834727
Sathya <sathya@laufers.net>
2013-05-22 05:32:44 -0700

    Bugfixes

--- HomeMatic Control/Peer.cpp
+++ HomeMatic Control/Peer.cpp
@@ -8,13 +8,13 @@ Peer::Peer(std::string serializedObject)
 	std::string entry;
 	address = std::stoll(serializedObject.substr(0, 8), 0, 16);
 	serialNumber = serializedObject.substr(8, 10);
-	firmwareVersion = std::stoll(serializedObject.substr(18, 2), 0, 16);
-	remoteChannel = std::stoll(serializedObject.substr(20, 2), 0, 16);
-	localChannel = std::stoll(serializedObject.substr(22, 2), 0, 16);
-	deviceType = (HMDeviceTypes)std::stoll(serializedObject.substr(24, 8), 0, 16);
-	messageCounter = std::stoll(serializedObject.substr(32, 2), 0, 16);
-	uint32_t configSize = (std::stoll(serializedObject.substr(34, 8)) * 16);
-	for(uint32_t i = 42; i < (42 + configSize); i += 16)
+	firmwareVersion = std::stoll(serializedObject.substr(18, 8), 0, 16);
+	remoteChannel = std::stoll(serializedObject.substr(26, 2), 0, 16);
+	localChannel = std::stoll(serializedObject.substr(28, 2), 0, 16);
+	deviceType = (HMDeviceTypes)std::stoll(serializedObject.substr(30, 8), 0, 16);
+	messageCounter = std::stoll(serializedObject.substr(38, 2), 0, 16);
+	uint32_t configSize = (std::stoll(serializedObject.substr(40, 8)) * 16);
+	for(uint32_t i = 48; i < (48 + configSize); i += 16)
 		config[std::stoll(serializedObject.substr(i, 8), 0, 16)] = std::stoll(serializedObject.substr(i + 8, 8), 0, 16);
 }
 
@@ -24,7 +24,7 @@ std::string Peer::serialize()
 	stringstream << std::hex << std::uppercase << std::setfill('0');
 	stringstream << std::setw(8) << address;
 	if(serialNumber.size() != 10) stringstream << "0000000000"; else stringstream << serialNumber;
-	stringstream << std::setw(2) << firmwareVersion;
+	stringstream << std::setw(8) << firmwareVersion;
 	stringstream << std::setw(2) << remoteChannel;
 	stringstream << std::setw(2) << localChannel;
 	stringstream << std::setw(8) << (int32_t)deviceType;
