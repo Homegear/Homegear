e8a38def9ef237929b75e6c87b99c0137ffc76c4
Sathya <sathya@laufers.net>
2013-05-24 04:42:37 -0700

    Removed old makefile

--- HomeMatic Control/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpthread -lxmlrpc_util -lxmlrpc_xmltok -lxmlrpc_xmlparse -lxmlrpc -lxmlrpc++ -lxmlrpc_abyss -lxmlrpc_server -lxmlrpc_server_abyss -lxmlrpc_server++ -lxmlrpc_server_abyss++ -lsqlite3 -lncurses
-
