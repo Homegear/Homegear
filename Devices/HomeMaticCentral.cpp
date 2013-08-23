2d2165606f13cbaeef2dd20f7882c586c75479d6
Sathya <sathya@laufers.net>
2013-08-23 21:11:37 +0200

    Bugfixes

--- Devices/HomeMaticCentral.cpp
+++ Devices/HomeMaticCentral.cpp
@@ -2302,11 +2302,11 @@ std::shared_ptr<RPC::RPCVariable> HomeMaticCentral::deleteDevice(std::string ser
 		}
 		//Force delete
 		if(force) deletePeer(address);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		while(_bidCoSQueueManager.get(address))
 		{
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 		if(!defer && !force && peerExists(address)) return RPC::RPCVariable::createError(-1, "No answer from device.");
 
