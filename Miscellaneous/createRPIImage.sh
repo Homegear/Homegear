1874d23d7a6aa834c75e2acf29af1994c36c9095
Sathya <sathya@laufers.net>
2013-11-21 20:52:03 +0100

    Added dependencies

--- Miscellaneous/createRPIImage.sh
+++ Miscellaneous/createRPIImage.sh
@@ -103,7 +103,7 @@ echo "#!/bin/bash
 debconf-set-selections /debconf.set
 rm -f /debconf.set
 apt-get update
-apt-get -y install locales console-common ntp openssh-server git-core binutils ca-certificates sudo parted
+apt-get -y install locales console-common ntp openssh-server git-core binutils ca-certificates sudo parted unzip p7zip-full
 wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update
 chmod +x /usr/bin/rpi-update
 mkdir -p /lib/modules
