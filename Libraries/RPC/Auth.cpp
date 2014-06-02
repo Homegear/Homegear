6cea3c76de5436e5b3e2c96aac664250b748bdc3
Sathya Laufer <sathya@laufers.net>
2014-06-02 12:31:44 +0200

    Error in basic auth "unauthorized" response

--- Libraries/RPC/Auth.cpp
+++ Libraries/RPC/Auth.cpp
@@ -87,6 +87,7 @@ void Auth::sendBasicUnauthorized(bool binary)
 			std::string content("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"><title>Unauthorized</title></head><body>Unauthorized</body></html>");
 			header.append("Content-Length: " + std::to_string(content.size()) + "\r\n\r\n");
 			header.append(content);
+			_basicUnauthHTTPHeader.reset(new std::vector<char>);
 			_basicUnauthHTTPHeader->insert(_basicUnauthHTTPHeader->begin(), header.begin(), header.end());
 		}
 		try
