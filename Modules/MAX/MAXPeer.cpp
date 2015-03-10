528eeb502c275af7c25eac6b4ba56fc0c6655f61
Sathya Laufer <sathya@laufers.net>
2015-03-10 18:43:38 +0100

    Smaller Queues for MAX!

--- Modules/MAX/MAXPeer.cpp
+++ Modules/MAX/MAXPeer.cpp
@@ -1090,9 +1090,6 @@ std::shared_ptr<BaseLib::RPC::Variable> MAXPeer::putParamset(int32_t clientID, i
 			if(changedParameters.empty() || changedParameters.begin()->second.empty()) return std::shared_ptr<BaseLib::RPC::Variable>(new BaseLib::RPC::Variable(BaseLib::RPC::VariableType::rpcVoid));
 
 			std::shared_ptr<MAXCentral> central = std::dynamic_pointer_cast<MAXCentral>(getCentral());
-			std::shared_ptr<PacketQueue> queue(new PacketQueue(_physicalInterface, PacketQueueType::CONFIG));
-			queue->noSending = true;
-			queue->peer = central->getPeer(_peerID);
 
 			bool firstPacket = true;
 			for(std::map<int32_t, std::map<int32_t, std::vector<uint8_t>>>::iterator i = changedParameters.begin(); i != changedParameters.end(); ++i)
@@ -1121,14 +1118,17 @@ std::shared_ptr<BaseLib::RPC::Variable> MAXPeer::putParamset(int32_t clientID, i
 
 				if(configPacket->payload()->size() > 2)
 				{
+					std::shared_ptr<PacketQueue> queue(new PacketQueue(_physicalInterface, PacketQueueType::CONFIG));
+					queue->noSending = true;
+					queue->peer = central->getPeer(_peerID);
 					queue->push(configPacket);
 					queue->push(central->getMessages()->find(DIRECTIONIN, 0x02, 0x02, std::vector<std::pair<uint32_t, int32_t>>()));
 					payload.clear();
 					setMessageCounter(_messageCounter + 1);
+					pendingQueues->push(queue);
 				}
 			}
 
-			pendingQueues->push(queue);
 			serviceMessages->setConfigPending(true);
 			if(!onlyPushing) central->enqueuePendingQueues(_address);
 			raiseRPCUpdateDevice(_peerID, channel, _serialNumber + ":" + std::to_string(channel), 0);
