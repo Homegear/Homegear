d2a466ef7cf640c636b454380daecf8629a567e1
Sathya Laufer <sathya@laufers.net>
2015-11-05 23:43:39 +0100

    Small changes

--- misc/Scripts/Docker Image Creation/PHP7 Build/CreateDockerPHPBuild.sh
+++ misc/Scripts/Docker Image Creation/PHP7 Build/CreateDockerPHPBuild.sh
@@ -196,6 +196,9 @@ rm php7*.tar.xz
 rm php7*.dsc
 cd php7*
 cd ext
+if [ test ! -f ext_skel.in ]; then
+	touch ext_skel.in
+fi
 wget https://github.com/krakjoe/pthreads/archive/v3.0.8.tar.gz
 tar -zxf v3.0.8.tar.gz
 rm v3.0.8.tar.gz
