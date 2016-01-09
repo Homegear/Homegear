a29df0fc9e80da36ea161e0ae16df03a73fa22e0
Sathya Laufer <sathya@laufers.net>
2016-01-09 10:57:40 +0100

    Memory debugging

--- misc/Scripts/CreateRPIImageJessie.sh
+++ misc/Scripts/CreateRPIImageJessie.sh
@@ -302,26 +302,27 @@ wget http://homegear.eu/downloads/nightlies/homegear-max_current_raspbian_jessie
 wget http://homegear.eu/downloads/nightlies/homegear-philipshue_current_raspbian_jessie_armhf.deb || exit 1
 wget http://homegear.eu/downloads/nightlies/homegear-sonos_current_raspbian_jessie_armhf.deb || exit 1
 dpkg -i libhomegear-base_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-homematicbidcos_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-homematicwired_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-insteon_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-max_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-philipshue_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 dpkg -i homegear-sonos_current_raspbian_jessie_armhf.deb
-apt-get -f install
+apt-get -y -f install
 service homegear stop
 echo \"Starting raspi-config...\"
 raspi-config
 rm /scripts/firstStart.sh
 rm -Rf /var/log/homegear/*
+echo \"Rebooting...\"
 reboot" >> scripts/firstStart.sh
 chown root:root scripts/firstStart.sh
 chmod 755 scripts/firstStart.sh
