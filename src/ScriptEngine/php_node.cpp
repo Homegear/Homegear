ebe7cf8f338f92bfc0d918b33cc7ec9e474c230b
Sathya Laufer <sathya@laufers.net>
2017-06-28 20:06:21 +0200

    Wait for waitForStop in PHP nodes

--- src/ScriptEngine/php_node.cpp
+++ src/ScriptEngine/php_node.cpp
@@ -475,6 +475,11 @@ bool php_init_node(PScriptInfo scriptInfo, zend_class_entry* homegearNodeClassEn
 				GD::out.printError("Error: Mandatory method \"stop\" not found in class \"HomegearNode\". File: " + scriptInfo->fullPath);
 				stop = true;
 			}
+			if(!zend_hash_str_find_ptr(&(homegearNodeClassEntry->function_table), "waitForStop", sizeof("stop") - 1))
+			{
+				GD::out.printError("Error: Mandatory method \"waitForStop\" not found in class \"HomegearNode\". File: " + scriptInfo->fullPath);
+				stop = true;
+			}
 		}
 		else if(scriptInfo->getType() == BaseLib::ScriptEngine::ScriptInfo::ScriptType::simpleNode)
 		{
