7f589dea3f058e17d87016002274bf2ade5c8a0f
Sathya Laufer <sathya@laufers.net>
2017-09-14 21:53:14 +0200

    Node debug status test is now URL encoded

--- src/Flows/FlowsClient.cpp
+++ src/Flows/FlowsClient.cpp
@@ -755,6 +755,8 @@ void FlowsClient::queueOutput(std::string nodeId, uint32_t index, Flows::PVariab
 			Flows::PVariable status = std::make_shared<Flows::Variable>(Flows::VariableType::tStruct);
 			std::string statusText = std::to_string(index) + ": " + message->structValue->at("payload")->toString();
 			if(statusText.size() > 20) statusText = statusText.substr(0, 17) + "...";
+			statusText = BaseLib::Http::encodeURL(statusText);
+			if(statusText.size() > 20) statusText = statusText.substr(0, 17) + "...";
 			status->structValue->emplace("text", std::make_shared<Flows::Variable>(statusText));
 			nodeEvent(nodeId, "statusTop/" + nodeId, status);
 		}
