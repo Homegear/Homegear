2fc464c253d1d1e3964a0fc19ce8c7dc28193724
Sathya Laufer <sathya@laufers.net>
2017-11-26 14:32:46 +0100

    Changed getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 dir=`mktemp -d`
 cat > "$dir/libhomegear-base-version.cpp" <<-'EOF'
-#include "homegear-base/BaseLib.h"
+#include <homegear-base/BaseLib.h>
 
 int main(int argc, char** argv)
 {
