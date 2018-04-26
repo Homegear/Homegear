43c53ebe5b0c0e8b33dfdef7bcdd01fee862bcc1
Sathya Laufer <sathya@laufers.net>
2018-04-27 00:24:05 +0200

    Changes to Docker node script

--- misc/Scripts/Docker Image Creation/Node Build/CreateDockerNodeBuild.sh
+++ misc/Scripts/Docker Image Creation/Node Build/CreateDockerNodeBuild.sh
@@ -257,15 +257,15 @@ unzip ${1}.zip
 rm ${1}.zip
 
 cd homegear-nodes-optional*
-./buildPackages <DISTVER> <ARCH>
+./buildPackages.sh <DISTVER> <ARCH>
 if [ "<ARCH>" == "amd64" ]; then
-	./buildPackages <DISTVER> all
+	./buildPackages.sh <DISTVER> all
 fi
 EOF
 chmod 755 $rootfs/build/CreateDebianPackage.sh
 sed -i "s/<DIST>/${dist}/g" $rootfs/build/CreateDebianPackage.sh
 sed -i "s/<DISTVER>/${distver}/g" $rootfs/build/CreateDebianPackage.sh
-sed -i "s/<ARCH>/${arch}/g" $rootfs/build/CreateDebianPackageNightly.sh
+sed -i "s/<ARCH>/${arch}/g" $rootfs/build/CreateDebianPackage.sh
 
 cat > "$rootfs/build/CreateDebianPackageNightly.sh" <<-'EOF'
 #!/bin/bash
@@ -281,7 +281,7 @@ fi
 
 /build/CreateDebianPackage.sh dev $1
 
-if test -f node-blue-*.deb; then
+if test -f /build/homegear-nodes-optional-dev/output/node-blue-*.deb; then
 	if test -f /build/UploadNightly.sh; then
 		/build/UploadNightly.sh
 	fi
@@ -300,7 +300,7 @@ DEBIAN_FRONTEND=noninteractive chroot $rootfs apt-get -y install libhomegear-nod
 
 /build/CreateDebianPackage.sh master $1
 
-if test -f node-blue-*.deb; then
+if test -f /build/homegear-nodes-optional-master/output/node-blue-*.deb; then
 	if test -f /build/UploadStable.sh; then
 		/build/UploadStable.sh
 	fi
@@ -413,11 +413,11 @@ echo "#!/bin/bash
 
 set -x
 
-cd /build
-if [ \$(ls /build | grep -c \"\\.changes\$\") -ne 0 ]; then
+cd /build/homegear-nodes-optional-master/output
+if [ \$(ls | grep -c \"\\.changes\$\") -ne 0 ]; then
 	path=\`mktemp -p / -u\`".tar.gz"
 	echo \"<DIST>\" > distribution
-	tar -zcpf \${path} homegear* lib* distribution
+	tar -zcpf \${path} *
 	if test -f \${path}; then
 		mv \${path} \${path}.uploading
 		filename=\$(basename \$path)
@@ -440,10 +440,11 @@ echo "#!/bin/bash
 
 set -x
 
-cd /build
-if [ \$(ls /build | grep -c \"\\.deb\$\") -ne 0 ]; then
+cd /build/homegear-nodes-optional-dev/output
+if [ \$(ls | grep -c \"\\.deb\$\") -ne 0 ]; then
 	path=\`mktemp -p / -u\`".tar.gz"
-	tar -zcpf \${path} *.deb
+	echo \"<DIST>\" > distribution
+	tar -zcpf \${path} *
 	if test -f \${path}; then
 		mv \${path} \${path}.uploading
 		filename=\$(basename \$path)
