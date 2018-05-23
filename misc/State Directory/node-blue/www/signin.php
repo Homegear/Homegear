a93e72e92d3fb34666b7f676b8d78ffcde7b0268
Sathya Laufer <sathya@laufers.net>
2018-05-23 12:51:14 +0200

    Updated Node-BLUE

--- misc/State Directory/node-blue/www/signin.php
+++ misc/State Directory/node-blue/www/signin.php
@@ -1,7 +1,7 @@
 <?php
 require_once("user.php");
 
-$loginResult = -1;
+$loginResult = -3;
 if(isset($_GET["logout"]))
 {
   $user = new User();
--- misc/State Directory/node-blue/www/static/debug/view/debug-utils.js
+++ misc/State Directory/node-blue/www/static/debug/view/debug-utils.js
@@ -222,7 +222,6 @@ RED.debug = (function() {
             clearMessageList(false);
         });
 
-
         return {
             content: content,
             footer: footerToolbar
@@ -238,6 +237,9 @@ RED.debug = (function() {
         workspaceOrder.forEach(function(ws,i) {
             workspaceOrderMap[ws] = i;
         });
+        candidateNodes = candidateNodes.filter(function(node) {
+            return workspaceOrderMap.hasOwnProperty(node.z);
+        })
         candidateNodes.sort(function(A,B) {
             var wsA = workspaceOrderMap[A.z];
             var wsB = workspaceOrderMap[B.z];
@@ -339,7 +341,7 @@ RED.debug = (function() {
                         activeMenuMessage.clearPinned();
                     }},
                     null,
-                    {id:"debug-message-menu-item-filter",label:RED._("node-red:debug.messageMenu.filterNode"),onselect:function(){
+                    {id:"debug-message-menu-item-filter", label:RED._("node-red:debug.messageMenu.filterNode"),onselect:function(){
                         var candidateNodes = RED.nodes.filterNodes({type:'debug'});
                         candidateNodes.forEach(function(n) {
                             filteredNodes[n.id] = true;
@@ -361,6 +363,15 @@ RED.debug = (function() {
             menuOptionMenu.on('mouseup', function() { $(this).hide() });
             menuOptionMenu.appendTo("body");
         }
+
+        var filterOptionDisabled = false;
+        var sourceNode = RED.nodes.node(sourceId);
+        if (sourceNode && sourceNode.type !== 'debug') {
+            filterOptionDisabled = true;
+        }
+        RED.menu.setDisabled('debug-message-menu-item-filter',filterOptionDisabled);
+        RED.menu.setDisabled('debug-message-menu-item-clear-filter',filterOptionDisabled);
+
         var elementPos = button.offset();
         menuOptionMenu.css({
             top: elementPos.top+"px",
@@ -392,12 +403,19 @@ RED.debug = (function() {
             $(msg).addClass('debug-message-hover');
             if (o._source) {
                 config.messageMouseEnter(o._source.id);
+                if (o._source._alias) {
+                    config.messageMouseEnter(o._source._alias);
+                }
             }
+
         };
         msg.onmouseleave = function() {
             $(msg).removeClass('debug-message-hover');
             if (o._source) {
                 config.messageMouseLeave(o._source.id);
+                if (o._source._alias) {
+                    config.messageMouseLeave(o._source._alias);
+                }
             }
         };
         var name = sanitize(((o.name?o.name:o.id)||"").toString());
