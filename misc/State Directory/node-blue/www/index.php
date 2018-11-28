6a0fbf81534d2d2f9ce8882418ed397ff9289b10
Sathya Laufer <sathya@laufers.net>
2018-11-28 21:06:58 +0100

    Updated Node-BLUE

--- misc/State Directory/node-blue/www/index.php
+++ misc/State Directory/node-blue/www/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once("user.php");
 
+if(!$_SERVER['WEBSOCKET_ENABLED']) die('WebSockets are not enabled on this server in "rpcservers.conf".');
+if($_SERVER['WEBSOCKET_AUTH_TYPE'] != 'session') die('WebSocket authorization type is not set to "session" in "rpcservers.conf".');
+
 $user = new User();
 if(!$user->checkAuth(true)) die();
 ?>
