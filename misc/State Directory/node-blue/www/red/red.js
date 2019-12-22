a58c9d12328060f5223db42d9e09004e5ff726d7
Dr. Sathya Laufer <s.laufer@homegear.email>
2019-12-22 11:40:43 +0100

    Updated Node-BLUE

--- misc/State Directory/node-blue/www/red/red.js
+++ misc/State Directory/node-blue/www/red/red.js
@@ -398,6 +398,7 @@ var RED = (function() {
                 }
                 node.statusTop = msg;
                 node.dirty = true;
+                node.dirtyStatus = true;
                 RED.view.redraw();
             }
         });
@@ -410,6 +411,7 @@ var RED = (function() {
                 }
                 node.statusBottom = msg;
                 node.dirty = true;
+                node.dirtyStatus = true;
                 RED.view.redraw();
             }
         });
@@ -30392,8 +30394,7 @@ RED.subflow = (function() {
         toolbar.empty();
 
         // Edit properties
-        // Hide edit button as this is not implemented in Homegear
-        $('<a class="button" style="display: none" id="red-ui-subflow-edit" href="#" data-i18n="[append]subflow.editSubflowProperties"><i class="fa fa-pencil"></i> </a>').appendTo(toolbar);
+        $('<a class="button" id="red-ui-subflow-edit" href="#" data-i18n="[append]subflow.editSubflowProperties"><i class="fa fa-pencil"></i> </a>').appendTo(toolbar);
 
         // Inputs
         $('<span style="margin-left: 5px;" data-i18n="subflow.input"></span> <div id="red-ui-subflow-input" style="display: inline-block;" class="button-group spinner-group">'+
@@ -30951,7 +30952,7 @@ RED.subflow = (function() {
                             class: "node-input-env-value",
                             type: "text",
                         }).attr("autocomplete","disable").appendTo(envRow)
-                        valueField.typedInput({default:'str',types:['str','num','bool','json','bin','env']});
+                        valueField.typedInput({default:'string',types:['bool','int','float','string','array','struct']});
                         valueField.typedInput('type', opt.parent?(opt.type||opt.parent.type):opt.type);
                         valueField.typedInput('value', opt.parent?((opt.value !== undefined)?opt.value:opt.parent.value):opt.value);
                     // }
@@ -30983,7 +30984,7 @@ RED.subflow = (function() {
                             icon: "",
                             label: {},
                             type: "input",
-                            opts: {types:['str','num','bool','json','bin','env']}
+                            opts: {types:['bool','int','float','string','array','struct']}
                         }
                         opt.ui.label = opt.ui.label || {};
                         opt.ui.type = opt.ui.type || "input";
@@ -31076,7 +31077,7 @@ RED.subflow = (function() {
          $('<div><i class="red-ui-editableList-item-handle fa fa-bars"></i></div>').appendTo(row);
 
          var typeOptions = {
-             'input': {types:['str','num','bool','json','bin','env']},
+             'input': {types:['bool','int','float','string','array','struct']},
              'select': {opts:[]},
              'spinner': {}
          };
@@ -31156,14 +31157,14 @@ RED.subflow = (function() {
                  {
                      value:"input",
                      label:RED._("editor.inputs.input"), icon:"fa fa-i-cursor",showLabel:false,multiple:true,options:[
-                         {value:"str",label:RED._("editor.types.str"),icon:"red/images/typedInput/az.svg"},
-                         {value:"num",label:RED._("editor.types.num"),icon:"red/images/typedInput/09.svg"},
+                         {value:"string",label:RED._("editor.types.string"),icon:"red/images/typedInput/az.svg"},
+                         {value:"int",label:RED._("editor.types.int"),icon:"red/images/typedInput/09.svg"},
+                         {value:"float",label:RED._("editor.types.float"),icon:"red/images/typedInput/09.svg"},
                          {value:"bool",label:RED._("editor.types.bool"),icon:"red/images/typedInput/bool.svg"},
-                         {value:"json",label:RED._("editor.types.json"),icon:"red/images/typedInput/json.svg"},
-                         {value: "bin",label: RED._("editor.types.bin"),icon: "red/images/typedInput/bin.svg"},
-                         {value: "env",label: RED._("editor.types.env"),icon: "red/images/typedInput/env.svg"}
+                         {value:"array",label:RED._("editor.types.array"),icon:"red/images/typedInput/array.svg"},
+                         {value:"struct",label:RED._("editor.types.struct"),icon:"red/images/typedInput/json.svg"}
                      ],
-                     default: ['str','num','bool','json','bin','env'],
+                     default: ['bool','int','float','string','array','struct'],
                      valueLabel: function(container,value) {
                          container.css("padding",0);
                          var innerContainer = $('<div>').css({
@@ -31408,21 +31409,21 @@ RED.subflow = (function() {
                     valueField.typedInput('types',ui.opts.types);
                     break;
                 case 'select':
-                   valueField.typedInput('types',['str']);
+                   valueField.typedInput('types',['string']);
                    break;
                  case 'checkbox':
                     valueField.typedInput('types',['bool']);
                     break;
                 case 'spinner':
-                    valueField.typedInput('types',['num'])
+                    valueField.typedInput('types',['int'])
                     break;
                 default:
-                    valueField.typedInput('types',['str','num','bool','json','bin','env'])
+                    valueField.typedInput('types',['bool','int','float','string','array','struct'])
              }
              if (ui.type === 'checkbox') {
                  valueField.typedInput('type','bool');
              } else if (ui.type === 'spinner') {
-                 valueField.typedInput('type','num');
+                 valueField.typedInput('type','int');
              }
              if (ui.type !== 'checkbox') {
                  checkbox = null;
@@ -31448,7 +31449,7 @@ RED.subflow = (function() {
         ui.label = ui.label||{};
         if (!ui.type) {
             ui.type = "input";
-            ui.opts = {types:['str','num','bool','json','bin','env']}
+            ui.opts = {types:['bool','int','float','string','array','struct']}
         } else {
             if (!ui.opts) {
                 ui.opts = (ui.type === "select") ? {opts:[]} : {};
@@ -31613,7 +31614,7 @@ RED.subflow = (function() {
                             }
                             switch (ui.type) {
                                 case "input":
-                                    if (JSON.stringify(ui.opts) === JSON.stringify({types:['str','num','bool','json','bin','env']})) {
+                                    if (JSON.stringify(ui.opts) === JSON.stringify({types:['bool','int','float','string','array','struct']})) {
                                         // This is the default input config. Delete it as it will
                                         // be applied automatically
                                         delete ui.type;
@@ -31700,7 +31701,7 @@ RED.subflow = (function() {
             var ui = data.ui || {};
             if (!ui.type) {
                 ui.type = "input";
-                ui.opts = {types:['str','num','bool','json','bin','env']}
+                ui.opts = {types:['bool','int','float','string','array','struct']}
             } else {
                 ui.opts = ui.opts || {};
             }
@@ -31714,16 +31715,16 @@ RED.subflow = (function() {
                             item.type = input.typedInput('type');
                         } else {
                             item.value = input.val();
-                            item.type = 'str';
+                            item.type = 'string';
                         }
                         break;
                     case "spinner":
                         item.value = input.val();
-                        item.type = 'num';
+                        item.type = 'int';
                         break;
                     case "select":
                         item.value = input.val();
-                        item.type = 'str';
+                        item.type = 'string';
                         break;
                     case "checkbox":
                         item.type = 'bool';
@@ -31930,7 +31931,7 @@ RED.userSettings = (function() {
     var viewSettings = [
         {
             options: [
-                {setting:"editor-language",local: true, label:"menu.label.view.language",options:function(done){ done([{val:'',text:RED._('menu.label.view.browserDefault')}].concat(RED.settings.theme("languages").map(localeToName).sort(compText))) }},
+                //{setting:"editor-language",local: true, label:"menu.label.view.language",options:function(done){ done([{val:'',text:RED._('menu.label.view.browserDefault')}].concat(RED.settings.theme("languages").map(localeToName).sort(compText))) }},
             ]
         },{
             title: "menu.label.view.grid",
