3322f45b38fd071156eae88d63efb96244054603
Dr. Sathya Laufer <s.laufer@homegear.email>
2020-01-20 22:39:36 +0100

    Restored correct makeDebug.sh

--- makeDebug.sh
+++ makeDebug.sh
@@ -9,5 +9,5 @@ SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 cd $SCRIPTDIR
 rm -Rf autom4te.cache
 ./bootstrap || exit 1
-./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --libdir=/usr/lib --without-scriptengine || exit 1
+./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --libdir=/usr/lib || exit 1
 make -j${BUILDTHREADS} CPPFLAGS="-DDEBUG" CXXFLAGS="-g -O0" && make install
