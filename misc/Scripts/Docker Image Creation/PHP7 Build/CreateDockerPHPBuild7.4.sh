aca235ab746c8f3c7f3ca30832cc7c4f57b1d7d2
Dr. Sathya Laufer <s.laufer@homegear.email>
2020-06-10 18:51:12 +0200

    Small fix

--- misc/Scripts/Docker Image Creation/PHP7 Build/CreateDockerPHPBuild7.4.sh
+++ misc/Scripts/Docker Image Creation/PHP7 Build/CreateDockerPHPBuild7.4.sh
@@ -292,7 +292,7 @@ cd ..
 mv php7* php7-homegear-dev-${version}
 tar -zcpf php7-homegear-dev_${version}.orig.tar.gz php7-homegear-dev-${version}
 cd php7-homegear-dev-*
-debuild -us -uc --no-lintian
+debuild --no-lintian -us -uc
 cd /
 rm -Rf /PHPBuild/php7-homegear-dev-${version}
 if test -f /PHPBuild/Upload.sh; then
