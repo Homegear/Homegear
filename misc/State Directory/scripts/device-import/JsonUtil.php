41101dbc7cbcd37f2c9a24c5598571cd1a6efe01
Dr. Sathya Laufer <s.laufer@homegear.email>
2022-07-11 11:40:59 +0200

    Added aggregated water meter

--- misc/State Directory/scripts/device-import/JsonUtil.php
+++ misc/State Directory/scripts/device-import/JsonUtil.php
@@ -6,18 +6,26 @@ class JsonUtil
      * From https://stackoverflow.com/a/10252511/319266
      * @return array|false
      */
-    public static function Load(string $filename): array|false
+    public static function Load(string $filename, bool $stripComments = false): array|false
     {
         $contents = @file_get_contents($filename);
         if ($contents === false) {
             return false;
         }
-        return json_decode(self::StripComments($contents), true);
+        if ($stripComments) {
+        	return json_decode(self::StripComments($contents), true);
+        } else {
+        	return json_decode($contents, true);
+        }
     }
 
-    public static function Decode(string $string): string
+    public static function Decode(string $string, bool $stripComments = false): string
     {
-        return json_decode(self::StripComments($string), true);
+    	if ($stripComments) {
+        	return json_decode(self::StripComments($string), true);
+        } else {
+        	return json_decode($string, true);
+        }
     }
 
     /**
